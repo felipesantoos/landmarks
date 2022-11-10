936edf69653878c3f0e9757bcd7e4ae17ff6cf90
Felipe da Silva Santos <felipedssantos811@gmail.com>
2022-11-10 12:41:05 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by user228370 on 11/10/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
